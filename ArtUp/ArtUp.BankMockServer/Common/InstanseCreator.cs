deb831fadfd98f23a31e53092a6e525f02f4f1e8
Yauheni Bychkouksi <Yauheni_bychkouski@epam.com>
2016-10-13 16:20:36 +0300

    Add class creator and project for the test the operation of bank

---
+++ ArtUp/ArtUp.BankMockServer/Common/InstanseCreator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtUp.BankMockServer.Common
+{
+    public static class InstanseCreator<T>
+    {
+        public static object GetInstance()
+        {
+            Type TestType = typeof(T);
+            
+            System.Reflection.ConstructorInfo ci1 = TestType.GetConstructor(new Type[] { });
+
+            return ci1 == null ? null : ci1.Invoke(new object[] { });
+        }
+
+        public static object GetInstance(string connectionString)
+        {
+            Type TestType = typeof(T);
+            
+            System.Reflection.ConstructorInfo ci2 = TestType.GetConstructor(new Type[] { typeof(string) });
+            
+            return ci2 == null ? null : ci2.Invoke(new object[] { connectionString });
+        }
+    }
+}
---
+++ ArtUp/BankMockServerTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArtUp.BankMockServer.Common;
+using ArtUp.BankMockServer;
+
+namespace BankMockServerTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = InstanseCreator<TestClass>.GetInstance("ottakot");
+
+            var z = a as TestClass;
+
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(z.TestString);
+            Console.ReadLine();
+        }
+    }
+
+    public class TestClass
+    {
+        public string TestString { get; set; }
+
+        public TestClass(string s)
+        {
+            TestString = s;
+        }
+    }
+}
