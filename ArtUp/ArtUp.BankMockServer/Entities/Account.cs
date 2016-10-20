3e8327f98ef8b5a63e717c76501a39ac053e44d6
Yauheni Bychkouksi <Yauheni_bychkouski@epam.com>
2016-10-20 12:34:43 +0300

    Some things

---
+++ ArtUp/ArtUp.BankMockServer/Entities/Account.cs
@@ -0,0 +1,30 @@
+﻿using ArtUp.BankMockServer.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArtUp.BankMockServer.Entities
+{
+    public class Account
+    {
+        public int Id { get; set; }
+
+        public string Nubmer { get; set; }
+
+        public AccountType Type { get; set; }
+
+        public FaceType FaceType { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PatronomicName { get; set; }
+
+        public string Adress { get; set; }
+
+        public float Money { get; set; }
+    }
+}
