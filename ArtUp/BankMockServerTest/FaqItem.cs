ea29f33ec9e8aae63a21159b0829c04707c8adf6
Yauheni Bychkouksi <Yauheni_bychkouski@epam.com>
2016-11-24 17:23:23 +0300

    Miss

--- ArtUp/BankMockServerTest/FaqItem.cs
+++
@@ -1,9 +0,0 @@
-namespace BankMockServerTest
-{
-	public sealed class FaqItem
-	{
-		public string Question { get; set; }
-
-		public string Answer { get; set; }
-	}
-}
--- ArtUp/BankMockServerTest/Position.cs
+++
@@ -1,9 +0,0 @@
-namespace BankMockServerTest
-{
-	public class Position
-	{
-		public int X { get; set; }
-
-		public int Y { get; set; }
-	}
-}
